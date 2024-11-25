b92c851e71abe3304e5d723dbebd991d198ac328
Aldo <Aldo.castillo.p@usach.cl>
2024-11-25 01:33:52 -0300

    :construction: FEATURE Se agrego la ruta "docs" para levantar la pagina correctamente

---
+++ docs/_next/static/chunks/app/layout-aeb61d8964e006fd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1443:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,4839,23)),Promise.resolve().then(n.t.bind(n,4735,23)),Promise.resolve().then(n.t.bind(n,9534,23)),Promise.resolve().then(n.t.bind(n,347,23))},347:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[953,312,441,517,358],()=>s(1443)),_N_E=e.O()}]);
